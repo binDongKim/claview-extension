19b3e27f3ed9d67d63546b953451a0761e9bfda9
Dongbin Kim <bindongkim@gmail.com>
2017-04-08 02:40:50 +0900

    1. Change ‘date’ property type into String 2. getEvaluations: Query evaluation data of today

--- app/models/evaluation.js
+++ app/models/evaluation.js
@@ -2,16 +2,16 @@ var mongoose = require('mongoose');
 
 var evaluationSchema = mongoose.Schema({
   userId : { type: String, required: true, unique: true },
-  date: Date,
+  date: String,
   result: String,
   opinion: String
 });
 
 // save student's evaluation result, opinion and the date
 evaluationSchema.statics.submitEvaluation = function(id, result, opinion) {
-  var Evaluation = mongoose.model('Evaluation', evaluationSchema);
   return new Promise(function(resolve, reject) {
-    Evaluation.update({ userId: id }, { result: result, opinion: opinion, date: Date.now() }).exec().then(function(data) {
+    var Evaluation = mongoose.model('Evaluation', evaluationSchema);
+    Evaluation.update({ userId: id }, { result: result, opinion: opinion, date: new Date().toISOString().slice(0,10) }).exec().then(function(data) {
       resolve('Success');
     }, function(err) {
       reject(err);
@@ -19,11 +19,24 @@ evaluationSchema.statics.submitEvaluation = function(id, result, opinion) {
   });
 };
 
+// get an evaluation with userId
 evaluationSchema.statics.getEvaluation = function(id) {
   return new Promise(function(resolve, reject) {
     var Evaluation = mongoose.model('Evaluation', evaluationSchema);
     Evaluation.findOne({ 'userId' : id }).then(function(evaluation) {
       resolve(evaluation);
+    }, function(err) {
+      reject(err);
+    });
+  });
+};
+
+// get all evaluations of today
+evaluationSchema.statics.getEvaluations = function() {
+  return new Promise(function(resolve, reject) {
+    var Evaluation = mongoose.model('Evaluation', evaluationSchema);
+    Evaluation.find({ date: new Date().toISOString().slice(0,10) }).exec().then(function(evaluations) {
+      resolve(evaluations);
     }, function(err) {
       reject(err);
     });
