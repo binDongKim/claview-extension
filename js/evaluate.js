80bb6dd6a2eccbebdd46da4259ecc7e5ff0bd27b
Dongbin Kim <bindongkim@gmail.com>
2017-05-06 15:40:01 +0900

    Handle the modal accordingly

--- js/evaluate.js
+++ js/evaluate.js
@@ -7,14 +7,15 @@ $(document).ready(function() {
   var userResult = $('input[name="result"]').val(); // good or bad
   if(userResult) {
     $('div[data-buttons="evaluate"] button[data-result=' + userResult + ']').addClass('active'); // active the button user clicked
-    $('div[data-buttons="evaluate"] button:not([data-result=' + userResult + '])').prop('disabled', true); // disable the other button
+    // $('div[data-buttons="evaluate"] button:not([data-result=' + userResult + '])').prop('disabled', true); // disable the other button
   }
   $('#opinionModal').on('shown.bs.modal', function(event) {
     $('#opinion').focus();
     var button = $(event.relatedTarget) // Button that triggered the modal
-    var result = button.data('result');
+    var result = button.data('result'); // good or bad
     var modal = $(this);
     modal.find('.result').val(result);
+    modal.find('.opinion').val(modal.find('.' + result).val());
     if(modal.find('.opinion').val()) {
       modal.find('.opinion').prop('readonly', true);
       modal.find('.submit-btn').prop('disabled', true);
@@ -22,6 +23,8 @@ $(document).ready(function() {
   });
   $('#opinionModal').on('hidden.bs.modal', function () {
     var modal = $(this);
+    modal.find('.opinion').prop('readonly', false);
+    modal.find('.submit-btn').prop('disabled', false);
     if(!(modal.find('.opinion').prop('readonly'))) {
       modal.find('.opinion').val('');
     }
